b7a2753750d5f6fa8ccf8577f6c1812270c4c910
David <david.scott.2875@gmail.com~>
2024-10-25 13:44:36 +0200

    added comments to class

--- grid.py
+++ grid.py
@@ -1,9 +1,19 @@
 class Grid:
-    def __init__(self, width, height) -> None:
+    def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
     
-    def position(self, x, y):
+    def position(self, x: int, y: int) -> tuple[int]:
+        """
+            Takes x and y coordinates and converts it to the torus planet
+
+            args
+            x coordinate which is an int
+            y coordinate which is an int
+
+            returns
+            tuple of x and y coordinate
+        """
         x % self.width
         y % self.height
         return (x,y)
