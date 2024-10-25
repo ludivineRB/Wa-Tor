5cf74d551ce1289dbdb2b8631877fd911295bd1e
flavigny maxime <maxime.mcfly@gmail.com>
2024-10-25 16:47:43 +0200

    2 commit

---
+++ fish.py
@@ -0,0 +1,34 @@
+# Class Fish Creation
+
+class Fish(grid):
+
+    def __init__(self, x_coordinate = int, y_coordinate = int, Reproductiontime) -> None:
+       
+        self.ReproductionTime = ReproductionTime
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        super().__init__(self, width: int, height: int)
+
+
+
+# method move  
+    for fish in list_of_fish
+
+
+
+    def move(self):
+
+self.reproduction = ReproductionTime + 1
+
+
+
+
+# method reproduction
+# reproduction and initialization of the counter when greater than _
+    def reproduction(self):
+
+        if ReproductionTime > 6:
+            ReproductionTime = 0
+
+        else:
+            ReproductionTime += 1              
