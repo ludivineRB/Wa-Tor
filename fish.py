6cd5946a75d1273ca84feccdb45998a3bb723a23
flavigny maxime <maxime.mcfly@gmail.com>
2024-10-27 22:39:54 +0100

    3rd commit

--- fish.py
+++ fish.py
@@ -2,33 +2,48 @@
 
 class Fish(grid):
 
-    def __init__(self, x_coordinate = int, y_coordinate = int, Reproductiontime) -> None:
-       
-        self.ReproductionTime = ReproductionTime
+    def __init__(self, x_coordinate:int, y_coordinate:int) -> None:
+        super().__init__(self, width, height, x_coordinate, y_coordinate, Reproductiontime)
+        
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
-        super().__init__(self, width: int, height: int)
-
-
+        
+    def move(self, x, y, dir_x, dir_y):
+        new_x = (x + dir_x) % self.width
+        new_y = (y + dir_y) % self.height
+        return new_x, new_y
 
 # method move  
-    for fish in list_of_fish
+#    for fish in list_of_fish
 
 
 
-    def move(self):
+    def move_fish(fish, grid, list_of_animals):
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        random.shuffle(directions)
 
-self.reproduction = ReproductionTime + 1
+        for dir_x, dir_y in directions:
+            new_x, new_y = Fish.move(fish.x, fish.y, dir_x, dir_y)
+            empty = True
+            for animals in list_of_animals:
+                if animals.x == new_x and animals.y == new_y:
+                    empty = False
+                    break
+            if empty:
+                x_old, y_old = fish.x_coordinate, fish.y_coordinate
+                fish.x_coordinate, fish.y_coordinate = new_x, new_y
+
+        fish.ReproductionTime += 1
 
 
 
 
 # method reproduction
 # reproduction and initialization of the counter when greater than _
-    def reproduction(self):
+#    def reproduction(self):
 
-        if ReproductionTime > 6:
-            ReproductionTime = 0
+#        if ReproductionTime > 6:
+#            ReproductionTime = 0
 
-        else:
-            ReproductionTime += 1              
+#        else:
+#            ReproductionTime += 1              
